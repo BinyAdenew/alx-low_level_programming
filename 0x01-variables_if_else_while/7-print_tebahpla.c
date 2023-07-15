0fdc1383c8d0869bf550fc7f9d5f57d5a7a53184
Biny Adenew <binyhood58@gmail.com>
2023-07-15 10:28:54 -0700

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,17 +2,17 @@
 
 /**
  * main - prints the lowercase alphabet in reverse,
- * 
- * Return: Always 0
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char letter;
+	char ch;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letter);
-	
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
 	putchar('\n');
-	
 	return (0);
 }
